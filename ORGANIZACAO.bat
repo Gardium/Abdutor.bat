9405c8ebfa0b973c42bc4e32b7d7751cbc634480
Gardium <53237987+Gardium@users.noreply.github.com>
2020-03-25 15:13:24 -0300

    Add files via upload

---
+++ ORGANIZACAO.bat
@@ -0,0 +1,42 @@
+md ORGANIZACAO
+
+cd ORGANIZACAO
+
+md DOCUMENTOS
+md IMAGENS
+md PSD's
+md CDR's
+md VIDEOS
+md ai's
+md COMPACTADOS
+md exe's
+md CERTIFICADOS
+md BASES
+md txts
+cd..
+:LOOP
+TIMEOUT /T 30
+move *.txt     organizacao\txts
+move *.sql     organizacao\bases   
+move *.pfx     organizacao\certificados
+move *.exe     organizacao\exe's
+move *.zip     organizacao\compactados
+move *.rar     organizacao\compactados
+move *.pptx    organizacao\documentos
+move *.pdf     organizacao\documentos
+move *.doc     organizacao\documentos
+move *.docx    organizacao\documentos
+move *.xls     organizacao\documentos
+move *.xlsx    organizacao\documentos
+move *.png     organizacao\imagens
+move *.jpg     organizacao\imagens
+move *.gif     organizacao\imagens
+move *.bmp     organizacao\imagens
+move *.jpeg    organizacao\imagens
+move *.psd     organizacao\PSD's
+move *.CDR     organizacao\CDR's
+move *.mp4     organizacao\videos
+move *.mkv     organizacao\videos
+move *.avi     organizacao\videos
+move *.ai      organizacao\Ai's
+goto :LOOP
---
+++ ORGANIZACAO.vbs
@@ -0,0 +1,2 @@
+Set WshShell = CreateObject("WScript.Shell")
+WshShell.Run "organizacao.bat", 0
